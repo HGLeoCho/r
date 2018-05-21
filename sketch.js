d650f79b69706d69a3e6d9696a8a2ace8e84a1e2
HoGyeong Leo Cho <chg8778@gmail.com>
2018-05-20 22:32:18 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,23 +3,23 @@
 
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
   background(0);
   stroke(255);
   noFill();
-  drawCircle(300, 300, 400);
+  drawCircle(500, 500, 600);
   noLoop();
 }
 
 function drawCircle(x, y, d) {
   ellipse(x, y, d);
   if (d > 2) {
-    let newD = d * random(0.25 * 0.84);
-    drawCircle(x + newD, y, newD);
-    drawCircle(x - newD, y, newD);
+    let newD = d * random(0.2, 0.85);
+    drawCircle(x + newD / 2, y, newD);
+    drawCircle(x - newD / 2, y, newD);
     //drawCircle(x, y + d * 0.5, d * 0.5);
   }
 }
